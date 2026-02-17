716f5495193147d74fc78d6ead976ec51c526754
O-Georgiadis <Odysseus.georgiadis.98@gmail.com>
2026-02-17 22:28:32 +0200

    minor changes

--- day25-us-states-game/main.py
+++ day25-us-states-game/main.py
@@ -23,10 +23,7 @@ while len(guessed_states) < 50:
         continue
 
     if answer_state == "exit":
-        missing_states = []
-        for state in  all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pd.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         print(missing_states)
