9ef0c26c0d8ff174e1229a3a43cb618ecbb4fd96
O-Georgiadis <odysseas.georgiadis.98@gmail.com>
2026-02-22 12:38:34 +0200

    day 30 changes

--- day26-NATO-alphabet/main.py
+++ day26-NATO-alphabet/main.py
@@ -1,8 +1,20 @@
+import pandas as pd
+
 df = pd.read_csv("nato_phonetic_alphabet.csv")
 
 new_dict = {row.letter:row.code for (index,row) in df.iterrows()}
 
-user_input = input("Enter a word: ").upper()
+def generate_phoenetic():
+    user_input = input("Enter a word: ").upper()
+    try:
+        user_output = [new_dict[letter] for letter in user_input]
 
-user_output = [new_dict[letter] for letter in user_input]
-print(user_output)
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phoenetic()
+
+    else:
+        print(user_output)
+
+
+generate_phoenetic()
