7fe174aef480d00999bfbe212c382403c96602f1
O-Georgiadis <odysseas.georgiadis.98@gmail.com>
2026-03-25 17:42:34 +0200

    day 64 top movies

--- day64-top-movies/main.py
+++ day64-top-movies/main.py
@@ -7,34 +7,119 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
+from pathlib import Path
+import os
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+TMDB_API_KEY = os.environ.get("DAY_64_TMDB_API_KEY")
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/original"
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
 # CREATE DB
+class Base(DeclarativeBase):
+    pass
+
+db_path = Path(__file__).parent/"instance/top-movies-collection.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
 
 # CREATE TABLE
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=True)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String(250), nullable=True)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+    
+
+
+
+with app.app_context():
+    db.create_all()
+
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your Rating Out of 10 e.g. 7.5")
+    review = StringField("Your Review")
+    submit = SubmitField("Done")
+
+class AddMovieForm(FlaskForm):
+    name = StringField("Movie Title")
+    submit = SubmitField("Add Movie")
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    result = db.session.execute(db.select(Movie))
+    all_movies = result.scalars().all()
 
+    all_movies.sort(key=lambda m: m.rating or 0, reverse=True)
+
+    for i, movie in enumerate(all_movies, start=1):
+        movie.ranking = i
+    db.session.commit()
+    return render_template("index.html", movies=all_movies)
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit_movie():
+    form = RateMovieForm()
+    movie_id = request.args.get("id")
+    movie_to_update = db.get_or_404(Movie, movie_id)
+    if form.validate_on_submit():
+        movie_to_update.rating = float(form.rating.data)
+        movie_to_update.review = form.review.data
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("edit.html", movie=movie_to_update, form=form)
+
+
+@app.route("/delete", methods=["GET"])
+def delete():
+    movie_id = request.args.get("id")
+    movie_to_delete = db.get_or_404(Movie, movie_id)
+    db.session.delete(movie_to_delete)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_movie():
+    form = AddMovieForm()
+    if form.validate_on_submit():
+        movie_title = form.name.data
+        response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": TMDB_API_KEY, "query": movie_title})
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
+    return render_template("add.html", form=form)
+        
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+
+        response = requests.get(movie_api_url, params={"api_key": TMDB_API_KEY, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"],
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for("edit_movie", id=new_movie.id))
 
 if __name__ == '__main__':
     app.run(debug=True)
